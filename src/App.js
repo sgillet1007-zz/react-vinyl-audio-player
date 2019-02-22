31c6eb44f36b6eb600a7cf80e8d6990c89c22bf8
sgillet1007 <samuel.p.gillet@gmail.com>
2019-02-21 21:53:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      playing: false
+    }
+  }
+
+  handleClick = () => this.setState({ playing: !this.state.playing})
+
   render() {
+    const isPlaying = this.state.playing ? 'spinning' : ''
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div onClick={this.handleClick} className="vinyl-wrapper">
+        <div className={`vinyl ${isPlaying}`} >TCT</div>
       </div>
     );
   }
